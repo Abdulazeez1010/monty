fe6bd65224971b4c2fdda600dbe077e3dfe12de8
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-24 10:08:51 -0700

    Modified the main.c file

--- main.c
+++ main.c
@@ -96,15 +96,15 @@ void push(stack_t **stack, unsigned int line_number)
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current;
+	stack_t *current, *temp;
 	(void) line_number;
 
 	current = *stack;
 	while (current != NULL)
 	{
 		printf("%d\n", current -> n);
-		/*temp = current;*/
+		temp = current;
 		current = current -> next;
-		/*free(temp);*/
+		free(temp);
 	}
 }
