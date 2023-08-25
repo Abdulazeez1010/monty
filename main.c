d25cd294786ba221581ed404ccbdedae1dae58bf
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-24 21:35:14 -0700

    Added the mul.c file and modified the div.c file

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 		{"nop", nop},
 		{"sub", sub},
 		{"divide", divide},
+		{"multiply", multiply},
 		{NULL, NULL}
 	};
 
--- mul.c
+++ mul.c
@@ -1,30 +1,25 @@
 #include "monty.h"
 
 /**
- * divide - The opcode div divides the second top element of the
+ * multiply - The opcode mul multiplies the second top element of the stack
+ * with the top element of the stack.
  * stack by the top element of the stack.
  * @stack : The stack
  * @line_number : Current line number
  */
-void divide(stack_t **stack, unsigned int line_number)
+void multiply(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top, *second;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	top = *stack;
 	second = top->next;
-	if (top->n == 0)
-	{
-		fprintf(stderr, "L%u: division by zero\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	second->n /= top->n;
+	second->n *= top->n;
 	second->prev = NULL;
 	*stack = second;
 	free(top);
