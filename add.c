d44a4f948024a89556dd680b2d34556b666d41f4
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-24 19:51:30 -0700

    Modified the add.c file

--- add.c
+++ add.c
@@ -8,6 +8,7 @@
 void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top, *second;
+
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short", line_number);
